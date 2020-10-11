81ba89f287b5b199fc3c4a2376363b5b84c28f0e
Wolfgang Stefani <63498809+wolfgang-stefani@users.noreply.github.com>
2020-10-11 15:10:30 +0200

    P2_new.ipynb added

--- line.py
+++ line.py
@@ -7,32 +7,31 @@ import math
 # Define a class to receive the characteristics of each line detection (object-oriented programming: line is a object)
 class Line():
     def __init__(self):
-        # was the line detected in the last iteration?
-        self.detected = False  
-        
-        # x values (x = Ay**2 + By + C) of the last n fits of the line
-        self.recent_xfitted = [] 
-        
-        #average x values of the fitted line over the last n iterations
-        self.bestx = None
-        
-        #polynomial coefficients (A, B, C ?) averaged over the last n iterations
-        self.best_fit = None
-        
-        #polynomial coefficients for the most recent fit
-        self.current_fit = [np.array([False])]  
-        
-        #radius of curvature of the line in some units (meters?)
-        self.radius_of_curvature = None 
-        
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = None 
-        
-        #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float') 
-        
+        # was the line detected in the last frame or not
+        self.detected = False
         #x values for detected line pixels
-        self.allx = None  
-        
+        self.cur_fitx = None
         #y values for detected line pixels
-        self.ally = None
+        self.cur_fity = None
+        # x values of the last N fits of the line
+        self.prev_fitx = []
+        #polynomial coefficients for the most recent fit
+        self.current_poly = [np.array([False])]
+        #best polynomial coefficients for the last iteration
+        self.prev_poly = [np.array([False])]
+
+    def average_pre_lanes(self):
+        tmp = copy(self.prev_fitx)
+        tmp.append(self.cur_fitx)
+        self.mean_fitx = np.mean(tmp, axis=0)
+
+    def append_fitx(self):
+        if len(self.prev_fitx) == N:
+            self.prev_fitx.pop(0)
+        self.prev_fitx.append(self.mean_fitx)
+
+    def process(self, ploty):
+        self.cur_fity = ploty
+        self.average_pre_lanes()
+        self.append_fitx()
+        self.prev_poly = self.current_poly
